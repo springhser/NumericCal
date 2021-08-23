fcfc7e13406af76172a6ea77d171ae10a588e3dc
springhser <chenjinbincjb@163.com>
2021-08-23 22:28:54 +0800

    add matrix file

---
+++ Matrix/Matrix.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-23 22:27:20
+ * @LastEditTime: 2021-08-23 22:27:21
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /NumericCal/Matrix/Matrix.cpp
+ */
---
+++ Matrix/Matrix.hpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-23 22:27:13
+ * @LastEditTime: 2021-08-23 22:27:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /NumericCal/Matrix/Matrix.hpp
+ */
